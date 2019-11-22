b72ab4f0d69a09f7ea16c345971ed28ad470b12c
AshesL <huancr@outlook.com>
2019-11-23 00:25:04 +0800

    更新renderbuffer

--- SoftwareRender/SoftwareRender/Graphics.cpp
+++ SoftwareRender/SoftwareRender/Graphics.cpp
@@ -42,3 +42,51 @@ void Graphics::clear_color(Uint32 color)
 {
 	_clear_color = color;
 }
+
+void Graphics::set_renderbuffer(RenderBuffer* buffer)
+{
+	_renderbuffer = buffer;
+}
+
+void Graphics::draw_primitive(Primitive primitive, int count, int)
+{
+
+}
+
+void Graphics::set_pixel(int x, int y, Uint32 color)
+{
+	if(_renderbuffer != nullptr)
+	{
+		_renderbuffer->set_pixel(x, y, color);
+		return;
+	}
+	int bpp = _surface->format->BytesPerPixel;
+	Uint8 *p = (Uint8 *)_surface->pixels + y * _surface->pitch + x * bpp;
+
+	switch (bpp) {
+	case 1:
+		*p = color;
+		break;
+
+	case 2:
+		*(Uint16 *)p = color;
+		break;
+
+	case 3:
+		if (SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+			p[0] = (color >> 16) & 0xff;
+			p[1] = (color >> 8) & 0xff;
+			p[2] = color & 0xff;
+		}
+		else {
+			p[0] = color & 0xff;
+			p[1] = (color >> 8) & 0xff;
+			p[2] = (color >> 16) & 0xff;
+		}
+		break;
+
+	case 4:
+		*(Uint32 *)p = color;
+		break;
+	}
+}
--- SoftwareRender/SoftwareRender/RenderBuffer.h
+++ SoftwareRender/SoftwareRender/RenderBuffer.h
@@ -1,8 +1,13 @@
 #pragma once
+extern "C" {
+#include "SDL.h"
+}
+
 class RenderBuffer
 {
 public:
 	RenderBuffer();
 	~RenderBuffer();
+	void set_pixel(int, int, Uint32);
 };
 
